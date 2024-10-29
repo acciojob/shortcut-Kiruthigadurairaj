abdfd9a5b4c46a03c8026af43daf4b8661f0bdea
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2024-10-29 16:18:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function shortcut(s1, s2) {
   // your code here
+	 if (s1 === "" || s2 === "") {
+        return "";
+    }
+    return s1[0] + s2[0];
 }
+
+// Examples
+console.log(shortcut("Amnesty", "International")); // Output: 'AI'
+console.log(shortcut("Hello", "world"));           // Output: 'Hw'
+console.log(shortcut("", "International"));        // Output: ''
+console.log(shortcut("Amnesty", ""));          
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
